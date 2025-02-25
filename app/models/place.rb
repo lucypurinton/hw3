20aa982d61a056e1d080bb51da35ed4fce1a52d3
Lucy Purinton <lucy.purinton@kellogg.northwestern.edu>
2025-02-25 03:28:30 +0000

    added locations to places data, don't know how to run that ruby file

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,21 @@
 class Place < ApplicationRecord
   # talks to the "places" table
+  
+  #insert new item into places table
+  new_place = Place.new
+  new_place["name"] = "Mexico City"
+  new_company.save
+
+  new_place2 = Place.new
+  new_place2["name"] = "Charleston"
+  new_company2.save
+
+  new_place3 = Place.new
+  new_place3["name"] = "Beijing"
+  new_company3.save
+
+  new_place4 = Place.new
+  new_place4["name"] = "Amsterdam"
+  new_company4.save
 end
+
